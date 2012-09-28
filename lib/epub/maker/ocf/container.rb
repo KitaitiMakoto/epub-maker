3a7025104806259ef18fe8ac11a53b06209ef971
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-09-28 23:22:47 +0900

    Open class instead of include module

--- lib/epub/maker/ocf/container.rb
+++ lib/epub/maker/ocf/container.rb
@@ -1,28 +1,20 @@
-require 'nokogiri'
 require 'epub/ocf/container'
 
 module EPUB
-  module Maker
-    module OCF
-      module Container
-        def to_xml
-          Nokogiri::XML::Builder.new do |xml|
-            xml.container('xmlns' => EPUB::NAMESPACES['ocf'], 'version' => '1.0') {
-              xml.rootfiles {
-                rootfiles.each do |rootfile|
-                  xml.rootfile('full-path' => rootfile.full_path, 'media-type' => rootfile.media_type)
-                end
-              }
-            }
-          end.to_xml
-        end
-      end
-    end
-  end
-
   class OCF
     class Container
-      include Maker::OCF::Container
+      def to_xml
+        Nokogiri::XML::Builder.new {|xml|
+          xml.container('xmlns' => EPUB::NAMESPACES['ocf'], 'version' => '1.0') {
+            xml.rootfiles {
+              rootfiles.each do |rootfile|
+                xml.rootfile('full-path' => rootfile.full_path,
+                             'media-type' => rootfile.media_type)
+              end
+            }
+          }
+        }.to_xml
+      end
     end
   end
 end
