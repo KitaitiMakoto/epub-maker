ea6c9b9da7d42095e640a1fdcf39d32dc8ebbb24
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-09-28 20:01:50 +0900

    Add a test for Maker::OCF::Container

--- test/test_maker.rb
+++ test/test_maker.rb
@@ -8,6 +8,24 @@ class TestMaker < Test::Unit::TestCase
     FileUtils::Verbose.rm @file if File.exist? @file
   end
 
+  def test_container
+    require 'epub/maker/ocf/container'
+    container = EPUB::OCF::Container.new
+    rootfile = EPUB::OCF::Container::Rootfile.new
+    rootfile.full_path = 'OPS/contents.opf'
+    rootfile.media_type = 'application/oebps-package+xml'
+    container.rootfiles << rootfile
+    expected = Nokogiri.XML(<<EOC)
+<?xml version="1.0"?>
+<container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
+  <rootfiles>
+    <rootfile full-path="OPS/contents.opf" media-type="application/oebps-package+xml" />
+  </rootfiles>
+</container>
+EOC
+    assert_equal expected.to_s, Nokogiri.XML(container.to_xml).to_s
+  end
+
   def test_make
     EPUB::Maker.make_from_directory 'test/fixtures/book'
     assert_path_exist @file
