55c58a82f47336540d5f65f39e1ca64236edf42a
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-04-01 08:55:44 +0900

    Refactor make_from_package_document

--- lib/epub/maker.rb
+++ lib/epub/maker.rb
@@ -18,24 +18,16 @@ module EPUB
       end
 
       def make_from_package_document(package_document_path, root_dir=nil)
-        path = Pathname.new(package_document_path)
-        package = EPUB::Parser::Publication.new(path.read, path.to_path).parse
-        root_dir = root_dir ? Pathname.new(root_dir)
-                            : path.common_prefix(package.manifest.items.collect {|item| item.href.to_s})
-        base_dir = path.dirname.relative_path_from(root_dir)
-        base_dir = Pathname.new('') if base_dir.to_s == '.'
-        rootfile_path = path.relative_path_from(root_dir)
-        container = OCF::Container.new
-        rootfile = OCF::Container::Rootfile.new(rootfile_path)
-        container.rootfiles << rootfile
-        filename = root_dir.to_s + '.epub'
-        FileUtils::Verbose.cp File.expand_path('../../../templates/template.epub', __FILE__), filename
-        Zip::Archive.open filename do |archive|
-          archive.add_buffer 'META-INF/container.xml', container.to_xml
-          archive.add_file rootfile_path.to_s, path.to_s
-          package.manifest.items.each do |item|
-            path_on_container = base_dir + item.href.to_s
-            path_on_file_system = root_dir + path_on_container
+        maker = new
+        maker.package_document_path = package_document_path
+        maker.root_dir = root_dir
+        FileUtils::Verbose.cp File.expand_path('../../../templates/template.epub', __FILE__), maker.output_path.to_s
+        Zip::Archive.open maker.output_path.to_s do |archive|
+          archive.add_buffer 'META-INF/container.xml', maker.container.to_xml
+          archive.add_file maker.rootfile_path.to_s, maker.package_document_path.to_s
+          maker.package.manifest.items.each do |item|
+            path_on_container = maker.base_dir + item.href.to_s
+            path_on_file_system = maker.root_dir + path_on_container
             archive.add_file path_on_container.to_s, path_on_file_system.to_s
           end
         end
@@ -44,11 +36,12 @@ module EPUB
       def make_from_directory(dir)
         dir = Pathname(dir)
 
-        container_file = dir + 'META-INF' + 'container.xml'
+        container_dir = dir + EPUB::Parser::OCF::DIRECTORY
+        container_file = container_dir + 'container.xml'
         ocf_parser = Parser::OCF.new(nil)
         container = ocf_parser.parse_container(container_file.read)
 
-        opf_file = dir+container.rootfile.full_path
+        opf_file = dir + container.rootfile.full_path
         publication_parser = Parser::Publication.new(opf_file.read, opf_file.to_s)
         package = publication_parser.parse
 
@@ -59,22 +52,19 @@ module EPUB
 
         Zip::Archive.open target_file.to_s do |zip|
           rootfile = Pathname(container.rootfile.full_path)
-          rootdir = rootfile.dirname
+          rootdir = dir + rootfile.dirname
           zip.add_file File.join('META-INF', 'container.xml'), container_file.to_s
           zip.add_file rootfile.to_s, opf_file.to_s
           files.each do |file|
-            zip.add_file (rootdir+file.href.to_s).to_s, file.href.to_s
+            puts "add_file #{file.href} #{rootdir+file.href}"
+            zip.add_file file.href.to_s, (rootdir+file.href.to_s).to_s
           end
         end
       end
     end
 
-    attr_reader :container, :package
-
-    def initialize
-      @container = OCF::Container.new
-      @package = Publication::Package.new
-    end
+    attr_reader :package_document_path
+    attr_writer :container, :package, :root_dir, :base_dir, :output_path
 
     def make
       yield self
@@ -82,6 +72,47 @@ module EPUB
       validate
       build_xml
       archive
+    end
+
+    def package_document_path=(path)
+      @package_document_path = path.kind_of?(Pathname) ? path : Pathname.new(path)
+    end
+
+    def container
+      @container ||= begin
+        c = OCF::Container.new
+        c.rootfiles << rootfile
+        c
+      end
+    end
+
+    def package
+      @package ||= EPUB::Parser::Publication.new(package_document_path.read, package_document_path.to_path).parse
+    end
+
+    def root_dir
+      @root_dir ||= package_document_path.common_prefix(package.manifest.items.collect {|item| item.href.to_s})
+    end
+
+    def root_dir=(path)
+      @root_dir = path.kind_of?(Pathname) ? path : Pathname.new(path)
+    end
+
+    def base_dir
+      bd = package_document_path.dirname.relative_path_from(root_dir)
+      bd.to_path == '.' ? Pathname.new('') : bd
+    end
+
+    def rootfile_path
+      package_document_path.relative_path_from(root_dir)
+    end
+
+    def rootfile
+      OCF::Container::Rootfile.new(rootfile_path)
+    end
+
+    def output_path
+      @output_path ||= Pathname.new(root_dir.to_s + '.epub')
     end
 
     def title=(title)
