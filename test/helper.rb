e97bd1f3a215d2b888680bdcc62a3dab394ffe9b
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-06-16 00:14:50 +0900

    Fix assert_valid_epub

--- test/helper.rb
+++ test/helper.rb
@@ -4,10 +4,13 @@ SimpleCov.start do
 end
 
 require 'test/unit/full'
+require 'open3'
+require 'shellwords'
 class Test::Unit::TestCase
   def assert_valid_epub(file)
     jar = File.join(ENV['GEM_HOME'], 'gems', 'epubcheck-3.0.0', 'lib', 'epubcheck-3.0', 'epubcheck-3.0.jar')
-    assert_true system('java', '-jar', jar, file)
+    stderr, status = Open3.capture2e "java -jar #{jar.shellescape} #{file.shellescape}"
+    assert_true (status.exitstatus == 0 or stderr !~ /^ERROR: /), stderr
   end
 end
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
