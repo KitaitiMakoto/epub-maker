57413bac4b39c29b344b7cb98acae8048bf20aa2
KITAITI Makoto <KitaitiMakoto@gmail.com>
2016-06-04 23:31:52 +0900

    Break lines for readability

--- lib/epub/maker.rb
+++ lib/epub/maker.rb
@@ -56,7 +56,9 @@ module EPUB
                   error.message,
                   "[#{self}]Working directory remained at: #{dir}"
                 ].join($RS))
-        backtrace.unshift("#{__FILE__}:#{__LINE__}:in `rescue in #{__method__}'"); error.set_backtrace backtrace; raise error
+        backtrace.unshift("#{__FILE__}:#{__LINE__}:in `rescue in #{__method__}'")
+        error.set_backtrace backtrace
+        raise error
       end
     end
   end
