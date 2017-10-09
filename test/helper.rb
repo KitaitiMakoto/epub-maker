a466f12e203757733ad4a4a5a654b0ffaf704eff
KITAITI Makoto <KitaitiMakoto@gmail.com>
2017-10-10 05:07:12 +0900

    Use EPUB::Parser.parse instead of EpubCheck on GitLab CI

--- test/helper.rb
+++ test/helper.rb
@@ -11,7 +11,14 @@ require 'pry'
 require 'epubcheck/ruby/cli'
 class Test::Unit::TestCase
   def assert_valid_epub(file)
-    assert_true Epubcheck::Ruby::CLI.new.execute(file)
+    if ENV["GITLAB_CI"]
+      warn "Validating EPUB file loosly by EPUB::Parser.parse instead of EpubCheck"
+      assert_nothing_raised do
+        EPUB::Parser.parse file
+      end
+    else
+      assert_true Epubcheck::Ruby::CLI.new.execute(file)
+    end
   end
 end
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
