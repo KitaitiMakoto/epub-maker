67b9abfe8bcdac0ca5881d4a3b5f26d5d1c46baa
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2020-02-01 01:35:07 +0900

    Add test cases for Metadata#modified=

--- test/test_maker_publication.rb
+++ test/test_maker_publication.rb
@@ -2,6 +2,7 @@
 require_relative 'helper'
 require 'epub/parser'
 require 'epub/maker/publication'
+require "date"
 
 class TestMakerPublication < Test::Unit::TestCase
   def setup
@@ -36,5 +37,26 @@ class TestMakerPublication < Test::Unit::TestCase
 
     media_type = doc.xpath('/opf:package/opf:bindings/opf:mediaType', EPUB::NAMESPACES).first
     assert_equal 'application/x-demo-slideshow', media_type['media-type']
+  end
+
+  def test_modified=
+    metadata = EPUB::Publication::Package::Metadata.new
+    metadata.modified = "2011-01-01T12:00:00Z"
+
+    assert_equal 1, metadata.metas.length
+    assert_equal "2011-01-01T12:00:00Z", metadata.modified.content
+
+    metadata.modified = Time.new(2020, 2, 1, 0, 0, 0, "+09:00")
+    assert_equal 1, metadata.metas.length
+    assert_equal "2020-01-31T15:00:00Z", metadata.modified.content
+
+    metadata.modified = Date.new(1993, 2, 24)
+    assert_equal 1, metadata.metas.length
+    expected = Time.new(1993, 2, 24, 0, 0, 0, Time.now.utc_offset)
+    assert_equal expected.utc.xmlschema, metadata.modified.content
+
+    metadata.modified = DateTime.new(1993, 2, 24, 0, 0, 0, "+00:00")
+    assert_equal 1, metadata.metas.length
+    assert_equal "1993-02-24T00:00:00Z", metadata.modified.content
   end
 end
