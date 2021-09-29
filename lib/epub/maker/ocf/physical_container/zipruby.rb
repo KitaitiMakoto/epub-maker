ca509c06a74fd7b11352f557fd7385663db5cc88
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2021-09-29 13:52:23 +0900

    Add mtime keyword argument to Zipruby adapter for interface compat

--- lib/epub/maker/ocf/physical_container/zipruby.rb
+++ lib/epub/maker/ocf/physical_container/zipruby.rb
@@ -4,7 +4,7 @@ module EPUB
   class OCF
     class PhysicalContainer
       class Zipruby < self
-        def write(path_name, content)
+        def write(path_name, content, mtime: nil)
           if @archive
             @archive.add_or_replace_buffer path_name, content
           else
