e949d24f7f6a447b1aa45b8f3e68dcef8b6f1e31
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2023-11-22 20:48:04 +0900

    Fix method to call

--- lib/epub/maker/ocf/physical_container/zipruby.rb
+++ lib/epub/maker/ocf/physical_container/zipruby.rb
@@ -8,7 +8,7 @@ module EPUB
           if @archive
             @archive.add_or_replace_buffer path_name, content
           else
-            open {|container| container.save(path_name, content)}
+            open {|container| container.write(path_name, content)}
           end
         end
       end
